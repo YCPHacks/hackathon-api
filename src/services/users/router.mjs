a06f1d5aff8de2ea406abb2bb86d4d1096a4d941
Ralph Greaves <rgreaves@ycp.edu>
2023-05-05 10:10:32 -0400

    Yeah, sure

---
+++ src/services/users/router.mjs
@@ -0,0 +1,90 @@
+/**
+ *
+ *  USERS ROUTER
+ *
+ */
+
+import express from 'express';
+
+import {
+  checkRequiredPermissions
+} from '../../middleware/auth0.mjs';
+
+import {
+  USERS_PERMISSIONS
+} from './permissions.mjs';
+
+import {
+  checkInUser,
+  createUser,
+  deleteUser,
+  listUsers,
+  readUser,
+  updateUser
+} from './service.mjs';
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  console.log(req.auth.payload);
+
+  next();
+});
+
+router.route('/users')
+  .get(
+    checkRequiredPermissions(USERS_PERMISSIONS.LIST),
+    async (req, res) => {
+      const result = await listUsers();
+
+      res.status(200).json(result);
+    })
+  .post(
+    checkRequiredPermissions(USERS_PERMISSIONS.CREATE),
+    async (req, res) => {
+      const result = await createUser(req.body.email, req.body.subject);
+
+      if (result.error) {
+        console.error(result.error);
+      }
+
+      res.status(201).json(result);
+    });
+
+router.route('/users/:user_id')
+  .delete(
+    checkRequiredPermissions(USERS_PERMISSIONS.DELETE),
+    async (req, res) => {
+      await deleteUser(req.params.user_id);
+
+      res.status(204).end();
+    })
+  .get(
+    checkRequiredPermissions(USERS_PERMISSIONS.READ),
+    async (req, res) => {
+      const result =
+          await readUser(req.params.user_id);
+
+      res.status(200).json(result);
+    })
+  .put(
+    checkRequiredPermissions(USERS_PERMISSIONS.UPDATE),
+    async (req, res) => {
+      await updateUser(req.params.user_id);
+
+      // TODO (REG): Finalize this HTTP status code.
+      res.status(200).end();
+    });
+
+router.route('/users/:user_id/.check_in')
+  .post(
+    checkRequiredPermissions(USERS_PERMISSIONS.CHECK_IN),
+    async (req, res) => {
+      const result = await checkInUser(req.params.user_id);
+
+      // TODO (REG): Finalize this HTTP status code.
+      res.status(200).json(result);
+    }
+  );
+
+export { router };
---
+++ src/services/users/service.mjs
@@ -0,0 +1,52 @@
+/**
+ *
+ *  USERS SERVICE
+ *
+ */
+
+import {
+  executeSQL
+} from '../../middleware/database.mjs';
+
+export async function checkInUser(userID) {
+  const [ error, data ] = await executeSQL(
+      'CALL check_in_user(?);',
+      userID);
+
+  return { data, error };
+}
+
+export async function createUser(email, subject) {
+  const [ error, data ] = await executeSQL(
+      'CALL create_user(?, ?);',
+      email,
+      subject);
+
+  return { data, error };
+}
+
+export async function deleteUser(userID) {
+  const [ error, data ] = await executeSQL(
+      'CALL delete_user(?);',
+      userID);
+
+  return { data, error };
+}
+
+export async function listUsers() {
+  const [ error, data ] = await executeSQL('CALL list_users;');
+
+  return { data, error };
+}
+
+export async function readUser(subject) {
+  const [ error, data ] = await executeSQL(
+      'CALL read_user(?);',
+      subject);
+
+  return { data, error };
+}
+
+export async function updateUser(userID) {
+  return;
+}
